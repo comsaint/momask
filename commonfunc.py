c713db58de6d7a5f382ccc364674c87400152c54
Long <long.pun@sands.com.mo>
2020-02-06 15:59:06 +0800

    change default access level in GCS to open access

--- commonfunc.py
+++ commonfunc.py
@@ -1,8 +1,11 @@
-def upload_to_gcs(project, src_file, dst_bucket, dst_blob_name):
+def upload_to_gcs(project, src_file, dst_bucket, dst_blob_name, make_public=True):
     from google.cloud import storage
     client = storage.Client(project=project)
     # https://console.cloud.google.com/storage/browser/[bucket-id]/
     bucket = client.get_bucket(dst_bucket)
     b = bucket.blob(dst_blob_name)
+    b.make_public()
     b.upload_from_filename(filename=src_file)
+    if make_public:
+        b.make_public()
     return 0
