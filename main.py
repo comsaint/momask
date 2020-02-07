4185a698b0765298fb93f27ab72f7fbece46a075
Long <long.pun@sands.com.mo>
2020-02-07 16:52:02 +0800

    fix a bug in viz_processing

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from scraper import run_scraper
 from rparser import run_parser
 from processing import run_processor
-from viz_processing import viz_processing
+from viz_processing import run_viz_processing
 from settings import DATA_FOLDER
-from settings import UPLOAD_TO_CLOUD, CLOUD_STORAGE_BUCKET, LIST_MODES
+from settings import UPLOAD_TO_CLOUD, CLOUD_STORAGE_BUCKET
 
 # create data folder
 DATA_FOLDER.mkdir(exist_ok=True)
@@ -11,7 +11,7 @@ DATA_FOLDER.mkdir(exist_ok=True)
 p_phq, p_hc, p_org = run_scraper()
 f_parsed = run_parser(path_to_phq=p_phq, path_to_hc=p_hc, path_to_org=p_org)
 run_processor()
-df = viz_processing()
+df = run_viz_processing()
 
 # upload the stock data to cloud storage for app to access
 if UPLOAD_TO_CLOUD is True:
