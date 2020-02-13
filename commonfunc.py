30d022b8009b597bbdcd6c0d99135bcfda326fe3
Long <long.pun@sands.com.mo>
2020-02-13 16:32:40 +0800

    change cache control of Blobs to no cache

--- commonfunc.py
+++ commonfunc.py
@@ -5,7 +5,7 @@ def upload_to_gcs(project, src_file, dst_bucket, dst_blob_name, make_public=True
     bucket = client.get_bucket(dst_bucket)
     b = bucket.blob(dst_blob_name)
     b.upload_from_filename(filename=src_file)
-    b.cache_control = 'public, max-age=300'  # change cache control
+    b.cache_control = 'no-cache, max-age=0'  # change cache control
     if make_public:
         b.make_public()
     return 0
