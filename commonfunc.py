4cac0358a5671a6b8fe8125be6e0bdf128035d39
Long <long.pun@sands.com.mo>
2020-02-14 22:10:35 +0800

    change cache control of Blobs

--- commonfunc.py
+++ commonfunc.py
@@ -4,7 +4,7 @@ def upload_to_gcs(project, src_file, dst_bucket, dst_blob_name, make_public=True
     # https://console.cloud.google.com/storage/browser/[bucket-id]/
     bucket = client.get_bucket(dst_bucket)
     b = bucket.blob(dst_blob_name)
-    b.cache_control = 'no-cache, max-age=0'  # change cache control
+    b.cache_control = 'must-revalidate, max-age=900'  # change cache control
     b.upload_from_filename(filename=src_file)
     if make_public:
         b.make_public()
